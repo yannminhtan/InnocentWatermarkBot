f239c99fa4624d19f40c6a31c0d1cef484f1fc55
yannminhtan <42146041+yannminhtan@users.noreply.github.com>
2025-06-27 20:05:20 +0630

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,11 +1,13 @@
 { pkgs }: {
   deps = [
-    pkgs.ffmpeg
-    pkgs.libjpeg
-    pkgs.zlib
-    pkgs.freetype
-    pkgs.python3
-    pkgs.python3Packages.setuptools
-    pkgs.python3Packages.wheel
+    pkgs.python311
+    pkgs.python311Packages.pillow
+    pkgs.python311Packages.moviepy
+    pkgs.python311Packages.numpy
+    pkgs.python311Packages.imageio
+    pkgs.python311Packages.tqdm
+    pkgs.python311Packages.setuptools
+    pkgs.python311Packages.pip
+    pkgs.ffmpeg_4
   ];
 }
